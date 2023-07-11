0e6f805c22d1454234c22ae757c961eabc0935be
Felipe Barreto <felipebarreto1050@gmail.com>
2023-07-11 11:10:11 -0300

    feat: intall axios

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,4 +1,4 @@
-import {  Platform, Alert } from 'react-native'
+import {  Platform } from 'react-native'
 import React from 'react'
 import * as C from '../styles/login.styles'
 import Input from '../../../shered/components/Inputs/Input'
@@ -7,19 +7,21 @@ import Text from '../../../shered/components/text/Text'
 import { textTypes } from '../../../shered/components/text/textTypes'
 import { theme } from '../../../shered/components/themes/theme/theme'
 import { Icon } from '../../../shered/components/icon/Icon'
+import axios from 'axios'
 
 
 const Login = () => {
-  const handleOnPress = () => {
-    Alert.alert("Clicou")
-  }
+  const handleOnPress = async () => {
+  const returnBD = await axios.get('http://192.168.1.102:8080/correios/01029-010')
+  console.log("🚀 ~ file: Login.tsx:17 ~ handleOnPress ~ returnBD:", returnBD.data)
 
+  }
   return (
     <C.ContainerLogin behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
       <C.Imagelog  source={require('../../../assets/images/logo.jpg')}/>
       <Input placeholder='Digite seu Email' title='Email'/>
       <Input secureTextEntry placeholder='Digite sua senha' title='Senha'/>
-      <Button disabled={false} loading={false} type={theme.buttons.BUTTONS_THEMES.primary} margin='8px' title='ENTRAR' onPress={handleOnPress}/>
+      <Button disabled={false} loading={false} type={theme.buttons.BUTTONS_THEMES.primary} margin=' 32px 8px' title='ENTRAR' onPress={handleOnPress}/>
     </C.ContainerLogin>
   )
 }
